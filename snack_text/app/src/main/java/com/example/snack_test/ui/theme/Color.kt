56532983a794bd75f53c9409e5ae348207fe5ffe
hyejingjung <ja03129@naver.com>
2022-01-11 18:18:54 +0900

    jetpack compose 기본

---
+++ snack_text/app/src/main/java/com/example/snack_test/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.example.snack_test.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+
+val bright_green = Color(0xFF43CF7F)
+val green = Color(0xFF03CF5C)
+val dark_green = Color(0xFF02883D)
---
+++ snack_text/app/src/main/java/com/example/snack_test/ui/theme/Theme.kt
@@ -0,0 +1,50 @@
+package com.example.snack_test.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val MyPalette = darkColors(
+    primary = green,
+    primaryVariant = dark_green,
+    secondary = bright_green
+)
+
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+private val LightColorPalette = lightColors(
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun Snack_testTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+    val colors = if (darkTheme) {
+        MyPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
